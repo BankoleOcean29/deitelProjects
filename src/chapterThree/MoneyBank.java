e9b03077047b042bb38ff2455e3ca1fb601a529d
BankoleOcean29 <bankolescripted@gmail.com>
2024-05-08 16:17:12 +0100

    New class demo

---
+++ src/chapterThree/MoneyBank.java
@@ -0,0 +1,10 @@
+package chapterThree;
+
+public class MoneyBank {
+    public static void main(String[] args) {
+
+
+
+        CustomerOfMoneyBank c1 = new CustomerOfMoneyBank("name", 34, 45, 45, 5);
+    }
+}

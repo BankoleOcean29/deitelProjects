75ed9d2cb4bf84de30d1bf26db4002dd3c628110
BankoleOcean29 <bankolescripted@gmail.com>
2024-06-27 20:24:22 +0100

    chapter three deitel updates

---
+++ src/chapterSeven/ArrayMain.java
@@ -0,0 +1,16 @@
+package chapterSeven;
+
+import java.util.Arrays;
+
+import static chapterSeven.SquareArray.squareArray;
+
+public class ArrayMain {
+
+    public static void main(String[] args) {
+        int[] result = squareArray();
+
+
+        System.out.println(Arrays.toString(result));
+    }
+
+}
---
+++ src/chapterSeven/SquareArray.java
@@ -0,0 +1,19 @@
+package chapterSeven;
+
+public class SquareArray {
+
+    public static int[] squareArray() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        //int[][][][][] myArray = new int[10][][][][];
+
+        int[] secondArray = new int[10];
+
+        /*for (int counter = 0; counter < secondArray.length; counter++) {
+            secondArray[counter] = counter + 1;
+        }*/
+
+        return secondArray;
+
+    }
+}
